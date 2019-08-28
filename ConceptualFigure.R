23a0551075405102c70b795420c0d292ae68d8c0
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-08-28 09:39:51 -0600

    Updated gauge numbers and dates

--- ConceptualFigure.R
+++ ConceptualFigure.R
@@ -21,33 +21,39 @@ FlowReverse<- "04087170" #Milwaukee
 ReverseStart<-as.Date("2018-12-15")
 ReverseEnd<-as.Date("2019-04-01")
 
+DataError<- "" #bypass through karst Palanisamy and Workman 2015 - need data or gage number
+DataErrorStart<-as.Date("")
+DataErrorEnd<-as.Date("")
+
 IsolatedPool<- "09512500" #Agua Fria River nr Mayer, AZ
 IsoStart<-as.Date("2019-06-01") 
 IsoEnd<-as.Date("2019-08-01") 
-
-SourceLoss<- "" #Woodforde, Australia -- need data for importing
-SourceStart<-as.Date("")
-SourceEnd<-as.Date("")
-
-AnthroSourceLoss<- ""
-ASourceStart<-as.Date("")
-ASourceEnd<-as.Date("")
-
-UpstreamLoss<- "06879650" #kings Creek nr Manhattan, KS
-UpStart<-as.Date("") #"2018-12-01 - when you zoom in it's actually at 1 cfs - better time-frame?
-UpEnd<-as.Date("") #"2018-07-01
-
-AnthroUpstreamLossGW<- ""  # Arkansas River near Dodge City - need the correct USGS gauge number
-AUpStart<-as.Date("") #2000-10-01 ?? 
-AUpEnd<-as.Date("") #2001-10-01
-
 
 Bypass<- "" #bypass through karst Palanisamy and Workman 2015 - need data or gage number
 BypassStart<-as.Date("")
 BypassEnd<-as.Date("")
 
-AnthroBypassA<- "08358400" #Rio Grand Floodway
-AnthroBypassB<- "08358300" #Rio Grand Conveyance
-ABypassStart<-as.Date("") #1991-06-01 Any more recent data that shows this? 
-ABypassEnd<-as.Date("") #1991-09-01
+#can probably remove this one
+UpstreamLoss<- "06879650" #kings Creek nr Manhattan, KS
+UpStart<-as.Date("") #"2018-12-01 - when you zoom in it's actually at 1 cfs - better time-frame?
+UpEnd<-as.Date("") #"2018-07-01
 
+AnthroUpstreamLossGW<- "07139500"  # Arkansas River near Dodge City - need the correct USGS gauge number
+AgwStart<-as.Date("2000-10-01") #2000-10-01 ?? 
+AgwEnd<-as.Date("2001-10-01") #2001-10-01
+
+
+AnthroConveyance<- "08358400" #Rio Grande Floodway
+AnthroConveyance<- "08358300" #Rio Grande Conveyance
+AConvStart<-as.Date("2005-08-01")
+AConvEnd<-as.Date("2005-11-01") 
+
+
+
+plot(date , Q_cms, type="n",
+     main = paste("Site", site_num),
+     xlab = "",
+     ylab = "Discharge (cfs)",
+     xaxt="n")
+axis.POSIXct(1, date, format="%b")
+axis.POSIXct(1, date, format="%Y", padj=1.8)
