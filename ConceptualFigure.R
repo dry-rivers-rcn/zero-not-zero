3704d10b869518f35ef06dc7a615e82dae31ab91
George H. Allen <georgehenryallen@gmail.com>
2019-09-24 11:07:32 -0600

    put all hydrographs on a single page

--- ConceptualFigure.R
+++ ConceptualFigure.R
@@ -3,7 +3,8 @@
 # Kendra Kaiser August 14th, 2019
 #kendra rocks
 #------------------------------------------------------------------
-
+# install.packages("dataRetrieval")
+# install.packages("tidyverse")
 library(dataRetrieval)
 library(tidyverse)
 pCode <- "00060"
@@ -126,3 +127,69 @@ plot(AnthroConveyanceBQ$dateTime, AnthroConveyanceBQ$Flow_Inst, type="l",
 axis.POSIXct(1, AnthroConveyanceBQ$dateTime, format="%b")
 axis.POSIXct(1, AnthroConveyanceBQ$dateTime, format="%Y", padj=1.8)
 dev.off()
+
+
+
+#-----------Plotting all on one page----------------------------
+
+pdfOutPath = "Figure3.pdf"
+pdf(pdfOutPath, width = 7.5, height = 7.5 )
+
+layout(matrix(1:6, nrow=3, byrow=T))
+par(mar=c(4,4,1,1))
+FlowReverseQ$Flow_Inst = FlowReverseQ$Flow_Inst*0.028316847
+plot(FrozenQ$dateTime, FrozenQ$Flow_Inst, 
+     type="l", col="blue", xlab="", ylab="Q (cms)", 
+     lwd=2, cex.main=0.75)
+lines(FrozenQ$dateTime[FrozenQ$Flow_Inst <= 0.001],
+      FrozenQ$Flow_Inst[FrozenQ$Flow_Inst <= 0.001], 
+      lty="dotted", col='white', lwd=3)
+axis.POSIXct(1, FrozenQ$dateTime, format="%Y", padj=1.8)
+
+FlowReverseQ$Flow_Inst = FlowReverseQ$Flow_Inst*0.028316847
+plot(FlowReverseQ$dateTime, FlowReverseQ$Flow_Inst, type="l", 
+     col="blue", xlab="", ylab="", lwd=1, cex.main=0.75)
+lines(FlowReverseQ$dateTime[FlowReverseQ$Flow_Inst <= 0], 
+      FlowReverseQ$Flow_Inst[FlowReverseQ$Flow_Inst <= 0], 
+      col='red', lwd=1)
+axis.POSIXct(1, FlowReverseQ$dateTime, format="%Y", padj=1.8)
+
+
+DataErrorQ$Flow_Inst = DataErrorQ$Flow_Inst*0.028316847
+plot(DataErrorQ$dateTime, DataErrorQ$Flow_Inst, type="l", main="", 
+     col="blue", xlab="", ylab="Q (cms)", lwd=2, cex.main=0.75)
+# lines(DataErrorQ$dateTime[DataErrorQ$Flow_Inst <= 0.001], 
+#       DataErrorQ$Flow_Inst[DataErrorQ$Flow_Inst <= 0.001], lty="dotted", col='white', lwd=3)
+axis.POSIXct(1, DataErrorQ$dateTime, format="%Y", padj=1.8)
+
+
+IsoQ$Flow_Inst = IsoQ$Flow_Inst*0.028316847
+plot(IsoQ$dateTime, IsoQ$Flow_Inst, type="l", main="",
+     col="blue", xlab="", ylab="", lwd=2, cex.main=0.75)
+# lines(IsoQ$dateTime[IsoQ$Flow_Inst <= 0.001], IsoQ$Flow_Inst[IsoQ$Flow_Inst <= 0.001], 
+#       lty="dotted", col='white', lwd=3)
+axis.POSIXct(1, IsoQ$dateTime, format="%Y", padj=1.8)
+
+
+AnthroGWQ$Flow_Inst = AnthroGWQ$Flow_Inst*0.028316847
+plot(AnthroGWQ$dateTime, AnthroGWQ$Flow_Inst, type="l", main="" ,
+     col="blue", xlab="Date", ylab="Q (cms)", lwd=2, cex.main=0.75)
+# lines(AnthroGWQ$dateTime[AnthroGWQ$Flow_Inst <= 0.001], AnthroGWQ$Flow_Inst[AnthroGWQ$Flow_Inst <= 0.001], lty="dotted", col='white', lwd=3)
+axis.POSIXct(1, AnthroGWQ$dateTime, format="%Y", padj=1.8)
+
+
+AnthroConveyanceAQ$Flow_Inst = AnthroConveyanceAQ$Flow_Inst*0.028316847
+plot(AnthroConveyanceAQ$dateTime, AnthroConveyanceAQ$Flow_Inst, type="l", main= "",
+     col="blue", xlab="Date", ylab="", lwd=2, cex.main=0.75)
+# lines(AnthroConveyanceAQ$dateTime[AnthroConveyanceAQ$Flow_Inst <= 0.001], AnthroConveyanceAQ$Flow_Inst[AnthroConveyanceAQ$Flow_Inst <= 0.001], lty="dotted", col='white', lwd=3)
+axis.POSIXct(1, AnthroConveyanceAQ$dateTime, format="%Y", padj=1.8)
+
+
+# plot(AnthroConveyanceBQ$dateTime, AnthroConveyanceBQ$Flow_Inst, type="l", 
+#      col="blue", xlab="Date", ylab="Q (cms)")
+# axis.POSIXct(1, AnthroConveyanceBQ$dateTime, format="%b")
+# axis.POSIXct(1, AnthroConveyanceBQ$dateTime, format="%Y", padj=1.8)
+
+dev.off()
+cmd = paste('open', pdfOutPath)
+system(cmd)
